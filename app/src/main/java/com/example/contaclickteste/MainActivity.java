78fd9b539fb6f056889a4a3319ef389f0111fb06
hannelymsk <maskehannely@gmail.com>
2025-05-07 16:26:19 -0300

    Initial commit

---
+++ app/src/main/java/com/example/contaclickteste/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.ContaClickTeste;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int contador = 0; // Variável para armazenar os cliques
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                contador++; // Incrementa o contador
+                textView.setText("Cliques: " + contador); // Atualiza o texto
+            }
+        });
+    }
+}
